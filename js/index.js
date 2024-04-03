5bf1e716df82fd721f352ec98fc15c7c9bb27e92
Ashit <ashit.kyros@gmail.com>
2024-04-03 18:41:34 +0530

    03april

--- js/index.js
+++ js/index.js
@@ -1,65 +1,151 @@
-// gsap.to(".box1", {
-//   x: 500,
-//   rotate: 360,
-//   borderRadius: 200,
-//   duration: 2,
-//   delay: 1,
-//   color: "blue",
-//   scrollTrigger: {
-//     trigger: "#page1",
-//     start: "top 0%",
-//     end: "bottom 60%",
-//     scrub: true,
-//   },
-// });
+function homepageAnimation() {
+  gsap.set(".slidesm", { scale: 5 });
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".home",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 2,
+    },
+  });
 
-// gsap.to(".box2", {
-//   rotate: 360,
-//   x: -800,
-//   y: -325,
-//   duration: 2,
-//   delay: 1,
-//   scrollTrigger: {
-//     trigger: "#page2",
-//     start: "top 0%",
-//     end: "bottom center",
-//     scrub: true,
-//   },
-// });
+  tl.to(
+    ".vdo_div",
+    {
+      "--clip": "0%",
+      ease: Power2,
+    },
+    "a"
+  )
 
-// gsap.to(".box3", {
-//   x: 800,
-//   duration: 2,
-//   delay: 1,
-//   scrollTrigger: {
-//     trigger: "#page3",
-//     start: "top 0%",
-//     end: "bottom 50%",
-//     scrub: true,
-//   },
-// });
+    .to(
+      ".slidesm",
+      {
+        scale: 1.5,
+        ease: Power2,
+      },
+      "a"
+    )
 
-// gsap.to(".extra h1", {
-//   transform: "translateX(-100%)",
-//   scrollTrigger: {
-//     trigger: ".extra",
-//     scroller: "body",
-//     start: "top 0%",
-//     end: "bottom -50%",
-//     scrub: 2,
-//     pin: true,
-//   },
-// });
+    .to(
+      ".lft",
+      {
+        xPercent: -10,
+        stagger: 0.03,
+        ease: Power4,
+      },
+      "b"
+    )
 
-// gsap.to(".innerslide", {
-    
-//   transform: "translateX(-100%)",
-//   scrollTrigger: {
-//     trigger: ".mainSlide",
-//     scroller: "body",
-//     start: "top 0%",
-//     end: "bottom -100%",
-//     scrub: 2,
-//     pin: true,
-//   },
-// });
+    .to(
+      ".rgt",
+      {
+        xPercent: 10,
+        stagger: 0.03,
+        ease: Power4,
+      },
+      "b"
+    );
+}
+
+function realPageAnimation() {
+  gsap.to(".slide", {
+    scrollTrigger: {
+      trigger: ".real",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 1,
+    },
+    xPercent: -200,
+    ease: Power4,
+  });
+}
+function TeamPageAnimation() {
+  document.querySelectorAll(".listelem").forEach((elem) => {
+    elem.addEventListener("mousemove", function (detas) {
+      gsap.to(this.querySelector(".picture"), {
+        x: gsap.utils.mapRange(0, window.innerWidth, -200, 200, detas.clientX),
+        opacity: 1,
+        ease: Power4,
+        duration: 0.5,
+      });
+    });
+    elem.addEventListener("mouseleave", function (detas) {
+      gsap.to(this.querySelector(".picture"), {
+        opacity: 0,
+        ease: Power4,
+        duration: 0.5,
+      });
+    });
+  });
+}
+function paraPageAnimation() {
+  var clutter = "";
+  document
+    .querySelector(".textpara")
+    .textContent.split("")
+    .forEach((e) => {
+      if (e === " ") clutter += `<span>&nbsp;</span>`;
+      clutter += `<span>${e}</span>`;
+    });
+  document.querySelector(".textpara").innerHTML = clutter;
+
+  gsap.set(".textpara span", {
+    opacity: 0.1,
+  });
+  gsap.to(".textpara span", {
+    opacity: 1,
+    stagger: 0.03,
+    ease: Power4,
+    scrollTrigger: {
+      trigger: ".para",
+      start: "top 60%",
+      end: "bottom 90%",
+      scrub: 2,
+    },
+  });
+}
+
+function lokoMotive() {
+  (function () {
+    const locomotiveScroll = new LocomotiveScroll();
+  })();
+}
+
+function capsulsAnimation() {
+  gsap.to(".capsul:nth-child(2)", {
+    scrollTrigger: {
+      trigger: ".capsules",
+      start: "top 70%",
+      end: "bottom bottom",
+      scrub: 1,
+    },
+    y: 0,
+    ease: Power4,
+  });
+}
+
+function bodyColorAnimation(){
+    document.querySelectorAll(".section").forEach(function (e) {
+        ScrollTrigger.create({
+          trigger: e,
+          start: "top 50%",
+          end: "bottom 50%",
+          onEnter: function () {
+            document.body.setAttribute("theme", e.dataset.color);
+          },
+          onEnterBack: function () {
+            document.body.setAttribute("theme", e.dataset.color);
+          },
+        });
+      });
+}
+
+
+homepageAnimation();
+realPageAnimation();
+TeamPageAnimation();
+paraPageAnimation();
+lokoMotive();
+capsulsAnimation();
+bodyColorAnimation();
